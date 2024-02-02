81fae0b33ba8773a44d72a1c9c9eaa6d02611e2b
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-02-02 15:18:03 +0300

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -4,5 +4,7 @@
 sudo apt install build-essential pkg-config cmake libssl-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+source "$HOME/.cargo/env"
+
 # install nushell
 cargo install nu
